48cf4dd6bc90777e3394c7479c63dc141acebec2
muhali16 <muhammadali55214@gmail.com>
2022-12-15 12:40:11 +0700

    Edit dashboard admin

--- admin/view/page_home.php
+++ admin/view/page_home.php
@@ -4,6 +4,19 @@
     <div class="card-body">
         <h5 class="card-title">Simple CRUD PHP Native dengan Tema Bela Negara</h5>
         <p class="card-text">Kita tidak boleh berhenti berkreasi, berinovasi, dan berprestasi. Kita harus buktikan ketangguhan kita. Kita harus menangkan masa depan kita dan kita wujudkan cita-cita para Pendiri Bangsa dengan semangat bela negara,</p>
-        <a href="page_about.php" class="btn btn-primary">Learn More</a>
+        <h2><strong>Daftar Anggota Kelompok 5</strong></h2>
+        <ol>
+            <li>Febriani ayu ardana- 77222341 - 77.9E2.07</li>
+            <li>Dwi Febriyanthi - 77222088 - 77.9E.07</li>
+            <li>Muhammad Rizki Aziz - 77222330 - 77.9E2.07</li>
+            <li>Reyhan Musalam Jessar - 77222331 - 77.9E2.07</li>
+            <li>Muhammad Fadilah - 77222377- 77.9E2.07</li>
+            <li>Muhammad Ali Mustaqim - 77222329 - 77.9E2.07</li>
+            <li>Putri Khairani - 77222096 - 77.9E.07</li>
+            <li>Krisna Dimas Pujiono -77222327 - 77.9E2.07</li>
+            <li>Rifaldi Firmansyah - 77222328 - 77.9E2.07</li>
+            <li>Tiara Suci Andini – 77222350 - 77.9E2.07</li>
+        </ol>
+        <a href="page_artikel.php" class="btn btn-primary">Learn More</a>
     </div>
 </div>
